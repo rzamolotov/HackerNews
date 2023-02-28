e00b7780b8fb777dd7566f674f811d943e2f1473
Roman Zamolotov <rzamolotov@gmail.com>
2023-02-28 22:05:04 +0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Роман Замолотов on 28.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
